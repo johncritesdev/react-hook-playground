0a4648e30bfa67076cf7a8bc6455620f479b74b5
John Crites <johncritesdev@gmail.com>
2021-01-01 10:00:22 -0500

    ...

--- react-hook-playground/src/index.js
+++ react-hook-playground/src/index.js
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  const [count, setCount] = useState(props.count);
 
 
   return (
     <div>
       <p>The current count is {count}</p>
       <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
       <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   )
+}
+
+App.defaultProps = {
+  count: 0
 }
 
 ReactDOM.render(
