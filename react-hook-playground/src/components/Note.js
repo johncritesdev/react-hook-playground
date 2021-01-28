69f8b26e73ded01ef219a9f4c341e3169eafefda
John Crites <johncritesdev@gmail.com>
2021-01-28 08:56:45 -0500

    ...

--- react-hook-playground/src/components/Note.js
+++ react-hook-playground/src/components/Note.js
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import NotesContext from '../context/notes-context';
+
+const useMousePosition = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  return position;
+}
 
 const Note = ({ note }) => {
 
   const { dispatch } = useContext(NotesContext);
+  const position = useMousePosition();
 
   return (
     <div>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <p>{position.x}, {position.y}</p>
       <button onClick={() => dispatch({ type: 'REMOVE_NOTE', title: note.title })}>x</button>
     </div>
   )
